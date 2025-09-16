692c0b3aef91141cd09490ae450ebef876786407
pahmatug81042 <ptug81042@yahoo.com>
2025-09-16 15:18:08 +0000

    chore: setup workoutController.js with required model imports

---
+++ controllers/workoutController.js
@@ -0,0 +1,6 @@
+const asyncHandler = require("express-async-handler");
+const Workout = require("../models/Workout");
+const Exercise = require("../models/Exercise");
+const User = require("../models/User");
+
+module.exports = {};
