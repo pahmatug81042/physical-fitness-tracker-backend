391140fddfbce0609558544dba37516ef8346fbe
pahmatug81042 <ptug81042@yahoo.com>
2025-09-16 14:34:20 +0000

    feat: implement user login in authController with password verification and JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
+const { isAxiosError } = require("axios");
 
 // @desc    Register new user
 // @route   POST /api/auth/register
@@ -30,4 +31,26 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { registerUser };
+// @desc    Authenticate user & get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+});
+
+module.exports = { registerUser, loginUser };
