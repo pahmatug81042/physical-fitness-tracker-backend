b633cb110d9f2c04b9d8142e95e340eaf34eef08
pahmatug81042 <ptug81042@yahoo.com>
2025-09-16 15:05:48 +0000

    feat: update apiClient.js with functions for exerciseController to fetch exercises by ID, body part, target, and equipment

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -1,19 +1,45 @@
 const axios = require("axios");
+require("dotenv").config();
 
-/**
- * Create a reusable Axios instance for RapidAPI requests
- * @param {string} host - RapidAPI Host (e.g., exercisedb.p.rapidapi.com)
- * @param {string} key - RapidAPI Key
- * @returns {AxiosInstance} Configured Axios client
- */
-const createApiClient = (host, key) => {
-    return axios.create({
-        baseURL: `https://${host}`,
-        headers: {
-            "X-RapidAPI-Host": host,
-            "X-RapidAPI-Key": key,
-        },
-    });
+const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+const EXERCISEDB_HOST = process.env.EXERCISEDB_HOST;
+
+const axiosInstance = axios.create({
+    baseURL: `https://${EXERCISEDB_HOST}`,
+    headers: {
+        "X-RapidAPI-Host": EXERCISEDB_HOST,
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+    },
+});
+
+// Fetch all exercises
+const getExercises = async () => {
+    const response = await axiosInstance.get("/exercises");
+    return response.data;
 };
 
-module.exports = createApiClient;
+// Fetch exercise by ID
+const getExerciseById = async (id) => {
+    const response = await axiosInstance.get(`/exercises/exercise/${id}`);
+    return response.data;
+};
+
+// Fetch exercises by body part
+const getExercisesByBodyPart = async (bodyPart) => {
+    const response = await axiosInstance.get(`/exercises/bodyPart/${bodyPart}`);
+    return response.data;
+};
+
+// Fetch exercises by target muscle
+const getExerciseesByTarget = async (target) => {
+    const response = await axiosInstance.get(`/exercises/target/${target}`);
+    response.data;
+};
+
+// Fetch exercises by equipment
+const getExercisesByEquipment = async (equipment) => {
+    const response = await axiosInstance.get(`/exercises/equipment/${equipment}`);
+    return response.data;
+};
+
+module.exports = { getExercises, getExerciseById, getExercisesByBodyPart, getExerciseesByTarget, getExercisesByEquipment };
